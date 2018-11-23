806f7001254c89843e6a36be8a668d68f81dc157
Benson Yu <bennywhyu@gmail.com>
2018-11-23 03:38:01 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,6 @@ def oxford_comma(array)
   elsif array.size == 2 
     array.join(" and ")
   elsif array.size >= 3
-    new_string= array.join(", ")
-    new_array = new_string.split
-    last_element = new_array.pop
-    new_array.insert(-1, "and").join(" ").push(last_element)
+    array.insert(-2, "and").join(", ")
   end
 end
